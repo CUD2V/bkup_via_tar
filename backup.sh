9f47f2d4294cba6a4453361f5bcf62a6bd1af8f8
seth <seth.russell@cuanschutz.edu>
2019-12-13 12:12:54 -0700

    Switching to pigz for parallel compression; writing file locally first, then copy to backup server

--- backup.sh
+++ backup.sh
@@ -6,8 +6,13 @@ yesterday=$(date -d "-1 day" +%Y-%m-%d)
 bkup_path=/root/backup/$(hostname)_backup
 mkdir -p $bkup_path
 
-archive=$bkup_path/$(hostname)-$today.tar.gz
-oldmetadata=$bkup_path/$(hostname)-$yesterday.metadata
+dest_path=/media/data_mount/backup/$(hostname)_backup
+mkdir -p $bkup_path
+
+afn=$(hostname)-$today.tar.gz
+archive=$bkup_path/$afn
+archivedest=$dest_path/$afn
+oldmetadata=$dest_path/$(hostname)-$yesterday.metadata
 newmetadata=$bkup_path/$(hostname)-$today.metadata
 
 logfile=$bkup_path/$(hostname)-$today.log
@@ -22,17 +27,39 @@ if [ "$(date +%A)" != "Sunday" ]; then
   fi
 fi
 
+
+# slower compression which should result in better compression doesn't make a noticiable difference in end file size
 tar \
   --exclude-from=tar_exclude \
   --exclude-tag-under=.exclude-under-from-tar-dump \
   --create \
   --preserve-permissions \
   --xattrs \
-  --gzip \
+  -I 'pigz -p 24' \
   --file=$archive \
   --listed-incremental=$newmetadata \
   --one-file-system \
   --ignore-failed-read \
   /
+
+cp $archive $archivedest
+cp $newmetadata $dest_path
+cp -rp $bkup_path/*.log $dest_path
+
+# due to recent issues with SMB mounted storage location, verify that copy worked
+file1sha=(`shasum -a 256 $archive`)
+file2sha=(`shasum -a 256 $archivedest`)
+
+if [ "$file1sha" == "$file2sha" ]
+then
+    echo "Local and Remote files have the same content - removing local"
+    echo $archivedest $file1sha
+    rm $archive
+    rm $newmetadata
+else
+    echo "Files are NOT the same"
+    echo $archive $file1sha
+    echo $archivedest $file2sha
+fi
 
 echo $(date)
